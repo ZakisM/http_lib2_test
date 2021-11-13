3b0f062bc857183a64dac78f8d20017a6c8c89ff
Zak <zakmikellides@gmail.com>
2021-11-13 21:23:44 +0000

    Added new handlers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use http_lib2::{response::ResponseBuilder, server::Server};
+use http_lib2::{request::Request, response::HttpResponse, server::Server};
 use std::error::Error;
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
@@ -6,15 +6,20 @@ type Result<T> = std::result::Result<T, Box<dyn Error>>;
 fn main() -> Result<()> {
     let mut server = Server::new([127, 0, 0, 1], 1234);
 
-    server
-        .at("/")
-        .get(|_| ResponseBuilder::new().body(b"Hello World").build());
-
-    server
-        .at("/Zak")
-        .get(|_| ResponseBuilder::new().body(b"Hello Zakis").build());
+    server.at("/").get(hello_world);
+    server.at("/vec_u8").get(vec_u8);
 
     server.start()?;
 
     Ok(())
+}
+
+fn hello_world(req: Request) -> impl HttpResponse {
+    "Hello World!"
+}
+
+fn vec_u8(req: Request) -> impl HttpResponse {
+    let hello_world_bytes = b"Hello World Bytes!";
+
+    hello_world_bytes.to_vec()
 }
